eb253d7dff5f3f160ed322fa8822822e9671ad44
Bobo <bnbsking@gmail.com>
2020-08-06 09:20:19 +0800

    Update and rename ftp.sh to auto-ftp.sh

---
+++ auto-ftp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HOST='172.18.70.88 21'
+USER='bnbsking'
+PASSWD="$1"
+
+cd ./test/crontabTrash
+for filename in *; do
+	ftp -n $HOST <<END_SCRIPT
+	quote USER $USER
+	quote PASS $PASSWD
+	put "$filename"
+	quit
+END_SCRIPT
+
+done
+
